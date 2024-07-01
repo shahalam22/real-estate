b97519b8cc6887b61d626c0530c7ec59c43ce348
SAAbir <shahalamabir21@gmail.com>
2024-07-01 14:17:42 +0600

    link update

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "https://real-estate-backend-8uov.onrender.com/api",
+    baseURL: "https://real-estate-backend-8uov.onrender.com",
     withCredentials: true,
 });
 
