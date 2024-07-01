b5c9f6d7abb86ff5930c9964ac68827900dd4f2e
SAAbir <shahalamabir21@gmail.com>
2024-07-01 14:39:18 +0600

    link update

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "https://real-estate-backend-8uov.onrender.com/api",
+    // baseURL: "https://real-estate-backend-8uov.onrender.com/api",
+    baseURL: "https://regal-brioche-8c4966.netlify.app/api",
     withCredentials: true,
 });
 
